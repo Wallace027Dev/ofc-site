3119e4d59613f549f2fdd425b7a78620a70cb8a3
Wallace Vieira <wallaceofc@hotmail.com>
2023-12-11 10:34:26 -0300

    feat: Contact Us form css

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,13 +1,13 @@
-import { Container } from "./styles";
+import Container, { Form } from "./styles";
 
 export default function ContactUs() {
   return (
-    <>
+    <Container>
       <h1>Faça um orçamento conosco</h1>
-      <Container>
+      <Form>
         <label htmlFor="fname">Nome</label>
         <input type="text" name="fname" id="fname" />
-        <label htmlFor="number">Número</label>
+        <label htmlFor="number">Número de telefone</label>
         <input type="tel" name="number" id="number" />
         <label htmlFor="fmail">Email</label>
         <input type="email" name="fmail" id="fmail" />
@@ -15,6 +15,7 @@ export default function ContactUs() {
         <input type="number" name="pieces-quatity" id="pieces-quatity" />
         <label htmlFor="location">Posição da peça</label>
         <select name="location" id="location">
+          <option value="" disabled selected></option>
           <option value="peito-padrao">Peito Esquerdo</option>
           <option value="peito-direito">Peito Direito</option>
           <option value="peito-centralizado">Peito Centralizado</option>
@@ -22,15 +23,18 @@ export default function ContactUs() {
           <option value="perna-bermuda">Perna de Bermuda</option>
           <option value="outro">Outras opções</option>
         </select>
+        <label htmlFor="photo-file">Sua logomarca</label>
+        <input type="file" name="photo-file" id="photo-file" />
         <label htmlFor="fmessage">Descreva mais sobre seu bordado</label>
         <textarea
           name="fmessage"
           id="fmessage"
-          cols="45"
-          rows="10"
+          cols="30"
+          rows="5"
           placeholder="Tamanhos aproximado, cores desejadas, posição desejada, etc."
         />
-      </Container>
-    </>
+        <button type="submit">ENVIAR</button>
+      </Form>
+    </Container>
   );
 }
--- src/pages/ContactUs/styles.js
+++ src/pages/ContactUs/styles.js
@@ -1,3 +1,69 @@
 import styled from "styled-components";
 
-export const Container = styled.form``;
+const Container = styled.main`
+  &,
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  align-items: center;
+
+  h1 {
+    font-size: 2.4rem;
+    font-weight: 700;
+    margin: 1.2rem auto;
+    text-align: center;
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 32rem;
+
+  label {
+    font-size: 1.6rem;
+    font-weight: 600;
+  }
+
+  input,
+  select,
+  textarea,
+  option {
+    padding: 0.25rem;
+    border: 1px solid var(--tertiary);
+    border-radius: 0.25rem;
+    font-size: 1.2rem;
+  }
+
+  label:not(:first-of-type),
+  button {
+    margin-top: 0.6rem;
+  }
+
+  input,
+  select,
+  button {
+    height: 3rem;
+  }
+
+  button {
+    background-color: var(--bt);
+    color: var(--bg);
+    border: none;
+    letter-spacing: 0.25rem;
+    font-weight: 600;
+    font-size: 1.6rem;
+    border-radius: 0.25rem;
+    cursor: pointer;
+
+    :hover {
+      background-color: var(--primary);
+      border-radius: 4rem;
+      transition: 0.4s;
+      color: var(--bt);
+      border: 1px solid var(--bt);
+      letter-spacing: 0.75rem;
+    }
+  }
+`;
+
+export default Container;
