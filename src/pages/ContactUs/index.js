170c69e1048b2d21996d1c7651452c1920668337
Wallace Vieira <wallaceofc@hotmail.com>
2023-12-11 15:57:25 -0300

    feat: sent the form (the input:file data is bugged)

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Container, { Form } from "./styles";
+import emailjs from "@emailjs/browser";
 
 export default function ContactUs() {
   const [name, setName] = useState("");
@@ -7,22 +8,65 @@ export default function ContactUs() {
   const [email, setEmail] = useState("");
   const [piecesQuantity, setPiecesQuantity] = useState("");
   const [location, setLocation] = useState("");
-  const [photoFile, setPhotoFile] = useState("");
   const [message, setMessage] = useState("");
+  const [photoFile, setPhotoFile] = useState("");
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setPhotoFile(selectedFile);
+  };
 
   function sendEmail(e) {
     e.preventDefault();
 
-    if (!(name, telNumber, email, piecesQuantity, location, photoFile)) {
-      alert("preencha os campos obrigatórios");
+    if (
+      !(name && telNumber && email && piecesQuantity && location && photoFile)
+    ) {
+      alert("Preencha todos os campos obrigatórios");
       return;
     }
+
+    const formData = new FormData();
+    formData.append("from_name", name);
+    formData.append("phone", telNumber);
+    formData.append("from_email", email);
+    formData.append("location", location);
+    formData.append("pieces_quantity", piecesQuantity);
+    formData.append("message", message);
+
+    // Adicionar o arquivo ao FormData
+    if (photoFile) {
+      formData.append("photo_file", photoFile);
+    }
+
+    emailjs
+      .sendForm(
+        "service_kqkdsvv",
+        "template_atmh9ut",
+        document.querySelector("form"),
+        "wKg0O3WLJGXQxw2sp"
+      )
+      .then(
+        (response) => {
+          console.log("Email enviado", response.status, response.text);
+          setName("");
+          setTelNumber("");
+          setEmail("");
+          setPiecesQuantity("");
+          setLocation("");
+          setMessage("");
+          setPhotoFile("");
+        },
+        (error) => {
+          console.log("ERROR:", error);
+        }
+      );
   }
 
   return (
     <Container>
       <h1>Faça um orçamento conosco</h1>
-      <Form onSubmit={sendEmail}>
+      <Form enctype="multipart/form-data" method="post" onSubmit={sendEmail}>
         <label htmlFor="fname">Nome *</label>
         <input
           name="fname"
@@ -64,18 +108,29 @@ export default function ContactUs() {
         />
 
         <label htmlFor="location">Posição da peça *</label>
-        <select name="location" id="location">
-          <option value="" disabled selected></option>
-          <option value="peito-padrao">Peito Esquerdo</option>
-          <option value="peito-direito">Peito Direito</option>
-          <option value="peito-centralizado">Peito Centralizado</option>
-          <option value="costa-ou-pala">Costa Centralizada / Pala</option>
-          <option value="perna-bermuda">Perna de Bermuda</option>
-          <option value="outro">Outras opções</option>
+        <select
+          name="location"
+          id="location"
+          onChange={(e) =>
+            setLocation(e.target.options[e.target.selectedIndex].text)
+          }
+          value={location || ""}
+        >
+          <option value="Peito Esquerdo">Peito Esquerdo</option>
+          <option value=">Peito Direito">Peito Direito</option>
+          <option value="Peito Centralizado">Peito Centralizado</option>
+          <option value="Costa">Costa Centralizada / Pala</option>
+          <option value="Perna de Bermuda">Perna de Bermuda</option>
+          <option value="Outras opções">Outras opções</option>
         </select>
 
-        <label htmlFor="photo-file">Sua logomarca *</label>
-        <input type="file" name="photo-file" id="photo-file" />
+        <label htmlFor="my_file">Sua logomarca *</label>
+        <input
+          type="file"
+          name="my_file"
+          id="my_file"
+          onChange={handleFileChange}
+        />
 
         <label htmlFor="fmessage">Descreva mais sobre seu bordado</label>
         <textarea
@@ -84,9 +139,11 @@ export default function ContactUs() {
           cols="30"
           rows="5"
           placeholder="Tamanhos aproximado, cores desejadas, posição desejada, etc."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
 
-        <button>ENVIAR</button>
+        <button type="submit">ENVIAR</button>
       </Form>
     </Container>
   );
